79be3b4af9a3015b90776408c177435d4f30256d
Kezhanguu Kruse <kezhanguukruse@gmail.com>
2026-07-11 21:08:58 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,16 +2,11 @@ import AuthLayout from "../components/auth/AuthLayout";
 import AuthLeft from "../components/auth/AuthLeft";
 import LoginForm from "../components/auth/LoginForm";
 
-interface LoginProps {
-  next: (username: string) => void;
-  createAccount: () => void;
-}
-
-export default function Login({ next, createAccount }: LoginProps) {
+export default function Login() {
   return (
     <AuthLayout
       left={<AuthLeft />}
-      right={<LoginForm next={next} createAccount={createAccount} />}
+      right={<LoginForm />}
     />
   );
 }
